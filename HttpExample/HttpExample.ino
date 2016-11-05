a2734f65a58ceb0917ca7afde46cb1afec2424b5
Antonio <carrasco.acd@gmail.com>
2016-11-05 15:46:33 +0100

    Updating the example in order to handle intervals of execution

--- HttpExample/HttpExample.ino
+++ HttpExample/HttpExample.ino
@@ -4,7 +4,23 @@
 HTTP http;
 Result result;
 
-void print(const char *message, int result = -1){
+unsigned long lastRunTime = 0;
+unsigned long waitForRunTime = 0;
+
+// the setup routine runs once when you press reset:
+void setup() {
+  Serial.begin(9600);
+  while(!Serial);
+  Serial.println("Starting!");
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  if (shouldTrackTimeEntry()) trackTimeEntry();
+}
+
+// functions
+void print(const char *message, long result = -1){
   if (result != -1){
     char buffer[100];
     sprintf(buffer, message, result);
@@ -15,46 +31,41 @@ void print(const char *message, int result = -1){
   }
 }
 
-// the setup routine runs once when you press reset:
-void setup() {
-  Serial.begin(9600);
-  while(!Serial);
-
-  //http.preInit();
+bool shouldTrackTimeEntry(){
+  unsigned long elapsedTime = millis() - lastRunTime;
+  print("Elapsed time: %ld", elapsedTime);
+  return elapsedTime >= waitForRunTime;
 }
 
-// the loop routine runs over and over again forever:
-void loop() {
+void trackTimeEntry(){
   
   print("Cofigure bearer: %d", http.configureBearer("movistar.es"));
   print("HTTP connect: %d", http.connect());
     
   char response[256];
   
-  result = http.get("smartgarden.herokuapp.com", response);
+  result = http.get("smartgarden.herokuapp.com/api", response);
   print("HTTP GET: %d", result);
   if (result == SUCCESS) {
     Serial.println(response);
     StaticJsonBuffer<200> jsonBuffer;
     JsonObject& root = jsonBuffer.parseObject(response);
-    Serial.print("date: ");
-    Serial.println((const char *)root["date"]);
-    Serial.print("name: ");
-    Serial.println((const char *)root["name"]);
+    lastRunTime = millis();
+    waitForRunTime = root["waitForRunTime"];
+    
+    print("Last run time: %ld", lastRunTime);
+    print("Next post in: %ld", waitForRunTime);
   }
-
-  result = http.post("smartgarden.herokuapp.com", "{\"name\":\"Antonio\"}", response);
+  
+  result = http.post("smartgarden.herokuapp.com/api", "{\"name\":\"Antonio\"}", response);
   print("HTTP POST: %d", result);
   if (result == SUCCESS) {
     Serial.println(response);
     StaticJsonBuffer<200> jsonBuffer;
     JsonObject& root = jsonBuffer.parseObject(response);
-    Serial.print("date: ");
-    Serial.println((const char *)root["date"]);
     Serial.print("name: ");
     Serial.println((const char *)root["name"]);
   }
 
   print("HTTP disconnect: %d", http.disconnect());
- 
 }
